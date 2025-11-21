0d2a4413f4f6dbff68fa306aa642ba7ec41fd08c
AxdaTFK <axldug5@gmail.com>
2025-11-20 20:07:06 -0500

    Ajustes

--- JS/Escenas carousel/mesa_apuestas.js
+++ JS/Escenas carousel/mesa_apuestas.js
@@ -1,16 +1,21 @@
 import * as THREE from 'three';
-import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 function crearEscena(divID, rutamodelo, x, y, z) {
+
+    let plane, mixer;
+    const clock = new THREE.Clock();
+
     const container = document.getElementById(divID);
 
     const scene = new THREE.Scene();
 
-    const camera = new THREE.PerspectiveCamera(75,container.clientWidth / container.clientHeight,0.1,1000);
+    const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
     camera.position.set(0, 1.5, 3);
 
     const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
     renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setAnimationLoop(animate);
 
     container.appendChild(renderer.domElement);
 
@@ -20,20 +25,26 @@ function crearEscena(divID, rutamodelo, x, y, z) {
     const directionalLight = new THREE.DirectionalLight(0x404040, 2);
     scene.add(directionalLight);
 
-    const loaderFBX = new FBXLoader();
-    loaderFBX.load(rutamodelo, function (object) {
-        object.scale.set(x, y, z);
-        object.position.set(0, 1, 0);
-        scene.add(object);
+    const loaderGLTF = new GLTFLoader();
+    loaderGLTF.load(rutamodelo, (gltf) => {
+        plane = gltf.scene;
+        plane.scale.set(x, y, z);
+        plane.position.set(0, 1, 0);
+        scene.add(plane);
+
+        if (gltf.animations.length) {
+            mixer = new THREE.AnimationMixer(plane);
+            mixer.clipAction(gltf.animations[0]).play();
+        }
     });
 
     function animate() {
+        if (mixer) mixer.update(clock.getDelta());
+
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
     }
     animate();
 }
 
-crearEscena(
-    'maquina_lasvegas','../../Archivos/models/source/maquina_lasvegas.fbx',0.02,0.02,0.02
-);
+crearEscena('maquina_lasvegas', './JS/Escenas_carousel/models/source/maquina_lasvegas.gltf', 0.02, 0.02, 0.02);
