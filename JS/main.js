cd436cbf22b188ca7b9b25b314638c8db78d5479
BockyMen <andresf.esquivia563@educacionbogota.edu.co>
2025-11-20 22:33:28 -0500

    footer arreglado

--- JS/main.js
+++ JS/main.js
@@ -10,13 +10,13 @@ renderer.setAnimationLoop( animate );
 contenedor.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xF54927 } );
+const material = new THREE.MeshBasicMaterial( { color: 0xBD2000 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 cube.position.set(-0.5,0,0)
 
 const geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
-const material2 = new THREE.MeshBasicMaterial( { color: 0xF54927 } );
+const material2 = new THREE.MeshBasicMaterial( { color: 0xBD2000 } );
 const cube2 = new THREE.Mesh( geometry2, material2 );
 scene.add( cube2 );
 cube2.position.set(0.5,0,0)
